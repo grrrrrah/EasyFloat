8a3f51562cd984f4e8aa25b9146f2089c91b71b5
Zhao-zi-long <lijiangop@163.com>
2021-11-26 16:23:18 +0800

    更新宽高

--- easyfloat/src/main/java/com/lzf/easyfloat/core/FloatingWindowHelper.kt
+++ easyfloat/src/main/java/com/lzf/easyfloat/core/FloatingWindowHelper.kt
@@ -402,4 +402,22 @@ internal class FloatingWindowHelper(val context: Context, var config: FloatConfi
             }
         }
     }
+    
+     /**
+     * 更新浮窗坐标和大小
+     */
+    fun updateFloatPositionSize(x: Int, y: Int, width: Int, height: Int) {
+        frameLayout?.let {
+            if (x == -1 && y == -1) {
+                // 未指定具体坐标，执行吸附动画
+                it.postDelayed({ touchUtils.updateFloat(it, params, windowManager) }, 200)
+            } else {
+                params.x = x
+                params.y = y
+                params.width = width;
+                params.height = height; 
+                windowManager.updateViewLayout(it, params)
+            }
+        }
+    }
 }
